f8e4b094eff1a8453f5e8a0ee70c9e680fa891df
Ricardo Uriegas <risardo01@gmail.com>
2025-03-30 20:02:57 -0600

    add_user.sql added

---
+++ backend/BD/add_users.sql
@@ -0,0 +1,22 @@
+
+-- This script inserts a new student
+INSERT INTO users (id, name, email, password_hash, role, avatar_url)
+VALUES (
+  'student',
+  'New student',
+  'nose@nose.com',
+  '$2a$12$8e5nXHLFkYk5dwcFs/mbsOZirgsO9ehACCWFETu6X3j/gODzZ5xa.', -- hash for 'nose123'
+  'student',
+  ''
+);
+
+-- This script inserts a new teacher
+INSERT INTO users (id, name, email, password_hash, role, avatar_url)
+VALUES (
+  'profe',
+  'New teacher',
+  'profe@nose.com',
+  '$2a$12$8e5nXHLFkYk5dwcFs/mbsOZirgsO9ehACCWFETu6X3j/gODzZ5xa.', -- hash for 'nose123'
+  'teacher',
+  ''
+);
